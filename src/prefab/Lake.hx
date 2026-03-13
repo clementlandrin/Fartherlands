711d66052dec3033cee0b1c326cffdfe066eb0de
lviguier <leo.vgr@hotmail.com>
2026-03-13 13:28:37 +0100

    Lake: new lake

--- src/prefab/Lake.hx
+++ src/prefab/Lake.hx
@@ -1,47 +1,193 @@
 package prefab;
 
-class LakeObject extends h3d.scene.Mesh {
+class LakeShader extends hxsl.Shader {
 
-	public function new( primitive, ?material, ?parent ) {
-		super(primitive, material, parent);
+	static var SRC = {
+
+		@global var global : {
+			var time : Float;
+			@perObject var modelViewInverse : Mat4;
+		};
+
+		@global var camera : {
+			var inverseViewProj : Mat4;
+		};
+
+		var relativePosition : Vec3;
+		var transformedPosition : Vec3;
+		var projectedPosition : Vec4;
+		var transformedNormal : Vec3;
+		var pixelColor : Vec4;
+
+		var metalness : Float;
+		var roughness : Float;
+		var occlusion : Float;
+		var emissive : Float;
+
+		@param var nearWaterColor : Vec3;
+		@param var middleWaterColor : Vec3;
+		@param var deepWaterColor: Vec3;
+		@param var roughnessValue : Float;
+		@param var opacityPower : Float;
+		@param var maxDepth: Float;
+
+		@param var colorNoiseTexture : Sampler2D;
+		@param var colorNoiseScale : Float;
+		@param var colorNoiseStrength : Float;
+
+		@param var normalMap : Sampler2D;
+		@param var waveIntensity : Float;
+		@param var waveScale : Float;
+		@param var waveSpeed : Float;
+		@param var secondWaveScale : Float;
+		@param var secondWaveRotate : Vec2;
+		@param var secondWaveSpeed : Float;
+
+		@global var depthMap : Channel;
+
+		var output : {
+			normal : Vec3,
+		};
+
+		function fragment() {
+			var waveUVBase = relativePosition.xy;
+			var waveUV = (waveUVBase + vec2(global.time * waveSpeed, 0.0)) * waveScale;
+			var waveUV2 = (waveUVBase + vec2(global.time * secondWaveSpeed) * secondWaveRotate) * secondWaveScale;
+
+			#if low_devices
+			var prescaledNormal = unpackNormal(vec4(normalMap.get(waveUV).rgb, 1.0));
+			#else
+			var prescaledNormal = unpackNormal(vec4(normalMap.get(waveUV).rgb, 1.0)) + unpackNormal(vec4(normalMap.get(waveUV2).rgb, 1.0));
+			#end
+			prescaledNormal.xy *= waveIntensity;
+			transformedNormal = prescaledNormal.normalize();
+
+			var screenPos = projectedPosition.xy / projectedPosition.w;
+
+			var depth = depthMap.get(screenToUv(screenPos));
+
+			var ruv = vec4( screenPos, depth, 1 );
+			var ppos = ruv * camera.inverseViewProj;
+			var wpos = ppos.xyz / ppos.w;
+			var waterDepth = distance(wpos.xyz, transformedPosition);
+			#if low_devices
+			var colorNoise = 0.0;
+			#else
+			var colorNoise = colorNoiseTexture.get(relativePosition.xy * colorNoiseScale).r;
+			colorNoise *= colorNoiseStrength;
+			#end
+
+			var p0 = 0.0;
+			var p1 = 0.6 + colorNoise ;
+			var p2 = 1.0 + colorNoise;
+			var t = saturate(1.0 - waterDepth / maxDepth);
+			var waterColor = mix(deepWaterColor, mix(middleWaterColor, nearWaterColor, smoothstep(p1, p2, t)), smoothstep(p0, p1, t));
+
+			pixelColor.rgb = waterColor;
+			metalness = 0.0;
+			roughness = roughnessValue;
+			occlusion = 1.0;
+			emissive = 0.0;
+			output.normal = transformedNormal;
+
+			var opacity = mix(0.2, 1.0, pow(1.0 - t, opacityPower));
+
+			pixelColor.a = opacity;
+		}
+	}
+}
+
+class LakeMesh extends h3d.scene.Mesh {
+	override function draw(ctx:h3d.scene.RenderContext) {
+		super.draw(ctx);
+		// cast(ctx.scene.renderer, gfx.Renderer).setEnabledSSR(true);
 	}
 }
 
 class Lake extends hrt.prefab.l3d.Polygon {
 
-	@:s public var refMatLib : String;
-	@:s public var overrides : Array<Dynamic> = [];
+	var lakeShader : LakeShader;
 
-	override function makeInstance() : Void {
-		var lo = new LakeObject(null, shared.current3d);
-		local3d = lo;
-		local3d.name = name;
-		updateInstance();
+	@:s public var nearWaterColor : Int = 0xffffff;
+	@:s public var middleWaterColor : Int = 0xffffff;
+	@:s public var deepWaterColor : Int = 0xffffff;
+	@:s public var roughness : Float = 0.0;
+	@:s public var opacityPower : Float = 5.0;
+	@:s public var maxDepth : Float = 5.0;
+
+	@:s public var colorNoiseTexture : String = null;
+	@:s public var colorNoiseScale : Float = 1.0;
+	@:s public var colorNoiseStrength : Float = 1.0;
+
+	@:s var normalMap : String = null;
+	@:s var waveIntensity : Float = 1.0;
+	@:s var waveScale : Float = 1.0;
+	@:s var waveSpeed : Float = 1.0;
+	@:s var secondWaveScale : Float;
+	@:s var secondWaveRotate : Float;
+	@:s var secondWaveSpeed : Float;
+
+	@:s public var collide : Bool = true;
+
+	override function makeObject( parent: h3d.scene.Object ) : h3d.scene.Object {
+		#if editor
+		hasDebugColor = false;
+		#end
+		var lo = new LakeMesh(null, parent);
+
+		lo.material.mainPass.setPassName("decal");
+		lo.material.mainPass.setBlendMode(Alpha);
+		lo.material.mainPass.depthWrite = false;
+		lo.material.castShadows = false;
+		lo.material.receiveShadows = true;
+
+		var depthPass = lo.material.allocPass("depthWrite", false);
+		depthPass.depthWrite = true;
+		depthPass.depthTest = Less;
+		depthPass.addShader(lo.material.mainPass.getShader(h3d.shader.BaseMesh));
+
+		var depthPrepass = lo.material.allocPass("depthPrepass", false);
+		depthPrepass.depthWrite = false;
+		depthPrepass.addShader(lo.material.mainPass.getShader(h3d.shader.BaseMesh));
+		depthPrepass.stencil = new h3d.mat.Stencil();
+		depthPrepass.stencil.setFunc(Always, (1 << 1), 0, (1 << 1));
+		depthPrepass.stencil.setOp(Keep, Keep, Replace);
+
+		lakeShader = new LakeShader();
+		lo.material.mainPass.addShader(lakeShader);
+		lo.material.mainPass.addShader(new h3d.shader.pbr.StrengthValues());
+
+		return lo;
 	}
 
 	override function updateInstance(?propName : String ) {
 		super.updateInstance(propName);
 
-		var lo : h3d.scene.Mesh = cast local3d;
+		if ( lakeShader != null ) {
+			lakeShader.nearWaterColor = h3d.Vector.fromColor(nearWaterColor);
+			lakeShader.middleWaterColor = h3d.Vector.fromColor(middleWaterColor);
+			lakeShader.deepWaterColor = h3d.Vector.fromColor(deepWaterColor);
+			lakeShader.roughnessValue = roughness;
+			lakeShader.opacityPower = opacityPower;
+			lakeShader.maxDepth = maxDepth;
 
-		if (this.refMatLib != null && this.refMatLib != "") {
-			var refMatLibPath = this.refMatLib.substring(0, this.refMatLib.lastIndexOf("/"));
-			var refMatName = this.refMatLib.substring(this.refMatLib.lastIndexOf("/") + 1);
-			var prefabLib = hxd.res.Loader.currentInstance.load(refMatLibPath).toPrefab();
+			lakeShader.colorNoiseTexture = colorNoiseTexture != null ? shared.loadTexture(colorNoiseTexture) : h3d.mat.Texture.fromColor(0);
+			lakeShader.colorNoiseTexture.wrap = Repeat;
+			lakeShader.colorNoiseScale = colorNoiseScale;
+			lakeShader.colorNoiseStrength = colorNoiseStrength;
 
-			try {
-				var mat = h3d.mat.Material.create();
-				if ( hxd.fmt.hmd.Library.setupMaterialLibrary( path -> return shared.loadTexture(path, false), mat, prefabLib, refMatName) )
-					lo.material = mat;
-			} catch( e : Dynamic ) {}
+			lakeShader.normalMap = normalMap != null ? shared.loadTexture(normalMap) : h3d.mat.Texture.fromColor(0x0000ff);
+			lakeShader.normalMap.wrap = Repeat;
+			lakeShader.waveIntensity = waveIntensity;
+			lakeShader.waveScale = waveScale;
+			lakeShader.waveSpeed = waveSpeed;
+			lakeShader.secondWaveScale = secondWaveScale;
+			lakeShader.secondWaveRotate.set(Math.cos(secondWaveRotate * Math.PI/180), Math.sin(secondWaveRotate * Math.PI/180));
+			lakeShader.secondWaveSpeed = secondWaveSpeed;
 		}
 	}
 
 	#if editor
-
-	override function setColor(color: Int) {
-		return;
-	}
 
 	override function getHideProps() : hide.prefab.HideProps {
 		return { icon : "square", name : "Lake" };
@@ -49,88 +195,46 @@ class Lake extends hrt.prefab.l3d.Polygon {
 
 	override function edit( ctx : hide.prefab.EditContext ) {
 		super.edit(ctx);
-
-		var matLibs = ctx.scene.listMatLibraries(this.getAbsPath());
-		var selectedLib = this.refMatLib == null ? null : this.refMatLib.substring(0, this.refMatLib.lastIndexOf("/"));
-		var selectedMat = this.refMatLib == null ? null : this.refMatLib.substring(this.refMatLib.lastIndexOf("/") + 1);
-		var materials = [];
-
-		var materialLibrary = new hide.Element('<div class="group" name="Material Library">
-		<dl>
-			<dt>Library</dt>
-			<dd>
-				<select class="lib">
-					<option value="">None</option>
-					${[for( i in 0...matLibs.length ) '<option value="${matLibs[i].name}" ${(selectedLib == matLibs[i].path) ? 'selected' : ''}>${matLibs[i].name}</option>'].join("")}
-				</select>
-			</dd>
-			<dt>Material</dt>
-			<dd>
-				<select class="mat">
-					<option value="">None</option>
-				</select>
-			</dd>
-		</dl></div>');
-
-		var libSelect = materialLibrary.find(".lib");
-		var matSelect = materialLibrary.find(".mat");
-
-		function updateLibSelect() {
-			libSelect.empty();
-			new hide.Element('<option value="">None</option>').appendTo(libSelect);
-
-			for (idx in 0...matLibs.length) {
-				new hide.Element('<option value="${matLibs[idx].name}" ${(selectedLib == matLibs[idx].path) ? 'selected' : ''}>${matLibs[idx].name}</option>');
-			}
-		}
-
-		function updateMatSelect() {
-			matSelect.empty();
-			new hide.Element('<option value="">None</option>').appendTo(matSelect);
-
-			materials = ctx.scene.listMaterialFromLibrary(this.getAbsPath(), libSelect.val());
-
-			for (idx in 0...materials.length) {
-				new hide.Element('<option value="${materials[idx].path + "/" + materials[idx].mat.name}" ${(selectedMat == materials[idx].mat.name) ? 'selected' : ''}>${materials[idx].mat.name}</option>').appendTo(matSelect);
-			}
-		}
-
-		function updateMat() {
-			var previousMatLib = refMatLib;
-			var mat = ctx.scene.findMat(materials, matSelect.val());
-			if ( mat != null ) {
-				this.refMatLib = Reflect.field(mat, "path") + "/" + Reflect.field(mat, "mat").name;
-				updateInstance();
-				ctx.rebuildProperties();
-			} else {
-				this.refMatLib = "";
-			}
-
-			ctx.properties.undo.change(Field(this, "refMatLib", previousMatLib), function() {
-				ctx.rebuildProperties();
-				updateInstance();
-			});
-		}
-
-		updateMatSelect();
-
-		libSelect.change(function(_) {
-			var previousMatSelect = matSelect.val();
-			updateMatSelect();
-
-			if (libSelect.val() == "" || previousMatSelect != "")
-				updateMat();
+		ctx.properties.add(new hide.Element('
+			<div class="group" name="Color">
+				<dl>
+					<dt>Near Water Color </dt><dd><input type="color" field="nearWaterColor"/></dd>
+					<dt>Middle Water Color</dt><dd><input type="color" field="middleWaterColor"/></dd>
+					<dt>Deep Water Color</dt><dd><input type="color" field="deepWaterColor"/></dd>
+					<dt>Roughness</dt><dd><input type="range" min="0" max="1" field="roughness"/></dd>
+					<dt>Opacity Power</dt><dd><input type="range" min="0" max="5" field="opacityPower"/></dd>
+					<dt>Lake max depth</dt><dd><input type="range" min="0" max="4" field="maxDepth"/></dd>
+				</dl>
+			</div>
+			<div class="group" name="Color Noise">
+				<dl>
+					<dt>Texture</dt><dd><input type="texturepath" field="colorNoiseTexture"/></dd>
+					<dt>Scale</dt><dd><input type="range" min="0" max ="1" step="0.01" field="colorNoiseScale"/></dd>
+					<dt>Strength</dt><dd><input type="range" min="0" max ="1" step="0.01" field="colorNoiseStrength"/></dd>
+				</dl>
+			</div>
+			<div class="group" name="Wave">
+				<dl>
+					<dt>NormalMap</dt><dd><input type="texturepath" field="normalMap"/></dd>
+					<dt>Wave Intensity</dt><dd><input type="range" min="0" max="10" field="waveIntensity"/></dd>
+					<dt>Wave Scale</dt><dd><input type="range" min="0" max="4" field="waveScale"/></dd>
+					<dt>Wave Speed</dt><dd><input type="range" min="0" max="1" field="waveSpeed"/></dd>
+					<dt>2d Wave Scale</dt><dd><input type="range" min="0" max="4" field="secondWaveScale"/></dd>
+					<dt>2d Wave Rotate</dt><dd><input type="range" min="-180" max="180" field="secondWaveRotate"/></dd>
+					<dt>2d Wave Speed</dt><dd><input type="range" min="0" max="1" field="secondWaveSpeed"/></dd>
+				</dl>
+			</div>
+			<div class="group" name="Collisions">
+				<dl>
+					<dt>Collide WorldMap</dt><dd><input type="checkbox" field="collide"/></dd>
+				</dl>
+			</div>
+			'), this, function(pname) {
+				ctx.onChange(this, pname);
 		});
-
-		matSelect.change(function(_) {
-			updateMat();
-		});
-
-		ctx.properties.add(materialLibrary, this);
 	}
 
 	#end
 
-	static var _ = hrt.prefab.Prefab.register("lake", Lake);
-
+	static var _ = hrt.prefab.Prefab.register("Lake", Lake);
 }
